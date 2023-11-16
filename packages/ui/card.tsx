7073519e19dd3a64cdab214d02fb1ca49f85b6d4
Ronald Cifuentes <ronald.cifuentes@acidlabs.com>
2023-11-16 09:07:02 -0500

    config

--- packages/ui/card.tsx
+++ packages/ui/card.tsx
@@ -1,16 +1,18 @@
-import * as React from "react";
+import React from "react";
+
+export interface CardProps {
+  className?: string;
+  title: string;
+  children: React.ReactNode;
+  href: string;
+}
 
 export function Card({
   className,
   title,
   children,
   href,
-}: {
-  className?: string;
-  title: string;
-  children: React.ReactNode;
-  href: string;
-}): JSX.Element {
+}: CardProps): JSX.Element {
   return (
     <a
       className={className}
@@ -18,9 +20,9 @@ export function Card({
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h2>
+      <h1>
         {title} <span>-&gt;</span>
-      </h2>
+      </h1>
       <p>{children}</p>
     </a>
   );
